61d7b117759fe81e01679a894ec34a3153591512
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2022-09-19 21:25:08 +0200

    events in vuejs

---
+++ script.js
@@ -0,0 +1,21 @@
+
+new Vue({
+    el: '#app',
+    data:{
+        counter:0,
+    },
+    methods:{
+        increment:function(){
+            this.counter++;
+        },
+        decrement:function(){
+            this.counter--;
+        },
+        increment2:function(step){
+            this.counter+=step;
+        },
+        decrement2:function(step){
+            this.counter-=step;
+        },
+    }
+})
